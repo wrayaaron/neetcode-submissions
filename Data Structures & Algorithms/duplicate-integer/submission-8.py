7103a34b301de1f410af61d381db985b5fd06a2a
Aaron Wray <67224605+wrayaaron@users.noreply.github.com>
2026-06-06 17:08:09 -0400

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,8 @@
+import numpy as np
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for num in range(1,len(nums)):
+            if nums[num] == nums[num - 1]:
+                return True
+        return False
